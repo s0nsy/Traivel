2500cdf3baf0e8004bb5ff1078032f411ee8c98f
namkungkang <namkung0131@naver.com>
2024-08-05 03:39:19 +0900

    1st generate

---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import buttonImg from "../assets/button.png";
+
+const FooterContainer = styled.div`
+  display: flex;
+  padding: 16px 380px 56px 380px;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const Input = styled.input`
+  display: flex;
+  width: 450px;
+  height: 62px;
+  margin-top: 200px;
+  padding: 0 200px;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 210px;
+  flex-shrink: 0;
+  border-radius: 36px;
+  border: 1px solid var(--Main_2, #01ecff);
+  background: rgba(255, 255, 255, 0.3);
+  box-shadow: 0px 4px 12px 2px rgba(1, 236, 255, 0.4);
+  &::placeholder {
+    color: #e6e6e6;
+    font-family: Pretendard;
+    font-size: 17px;
+    text-align: center;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+`;
+
+const Button = styled.img`
+  cursor: pointer;
+  z-index: +2;
+  position: absolute;
+  margin-left: 770px;
+  margin-top: 200px;
+`;
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <Input placeholder="루트포터의 질문에 답하고 완벽한 여행 계획을 세워보세요." />
+      <Button src={buttonImg}></Button>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import Icon from "../assets/Icon.svg";
+import Vector from "../assets/Vector.svg";
+import styled from "styled-components";
+import { useState } from "react";
+
+const Body = styled.div`
+  margin-top: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40vh;
+  flex-direction: column;
+  position: relative;
+`;
+
+const IconImg = styled.img`
+  width: 85px;
+  height: 80px;
+  margin-top: 25px;
+  filter: drop-shadow(0px 4px 4px rgba(1, 236, 254, 0.2));
+  z-index: 1;
+`;
+
+const VectorImg = styled.img`
+  position: absolute;
+  z-index: -1;
+  width: 700px;
+  height: 520px;
+  margin-right: 45px;
+  margin-top: 50px;
+  display: block;
+`;
+
+const Recommend = styled.p`
+  color: var(--White, #fff);
+  font-family: Pretendard;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+const Suggest = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 25px;
+`;
+
+const Button = styled.button`
+  padding: 20px;
+  border-radius: 20px;
+  border: 1px solid var(--Main_2, #01ecff);
+  background: rgba(0, 0, 0, 0.1);
+  display: flex;
+  width: 125px;
+  height: 100px;
+  padding: 0px 22px;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  color: var(--White, #fff);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  cursor: pointer;
+`;
+
+const Guide = styled.p`
+  color: var(--White, #fff);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+function Main() {
+  const [sentense] = useState([
+    "여행 테마",
+    "선호하는 지역",
+    "이동 및 경비",
+    "여행 취향",
+  ]);
+
+  return (
+    <Background>
+      <Body>
+        <IconImg src={Icon} alt="icon" />
+        <VectorImg src={Vector} alt="vector" />
+        <Recommend>대화로 추천받는 내 여행지</Recommend>
+        <Suggest>
+          {sentense.map((a, i) => {
+            return <Button>{sentense[i]}</Button>;
+          })}
+        </Suggest>
+        <Guide>멋진 일정을 계획하기 위해 루트포터 가이드를 이용해보세요.</Guide>
+      </Body>
+    </Background>
+  );
+}
+
+export default Main;
