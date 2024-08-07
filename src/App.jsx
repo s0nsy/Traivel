52e737e9760a4d245a91b5b038e775496ceb3504
jenklm <klmheewon@icloud.com>
2024-08-07 16:29:28 +0900

    feat: router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import Header from "./components/Header";
 import Body from "./components/Body";
 import List from "./Pages/List";
@@ -10,12 +11,18 @@ import Output from "./components/Output";
 import People from "./Pages/People";
 import { Routes, Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
+import React from 'react';
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./libs/router";
+
 
 const Global = createGlobalStyle`
  body {;
     background: var(--chat-style, linear-gradient(180deg, #001516 0%, #084B50 100%));
 `;
 function App() {
+
   return (
     <>
     <Global/>   
@@ -114,6 +121,8 @@ function App() {
     </>
 
   );
+    return <RouterProvider router={router} />;
+
 }
 
 
