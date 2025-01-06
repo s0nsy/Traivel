705ed8ba0b13214c3d587bb16a7f39a04350e512
namkungkang <namkung0131@naver.com>
2025-01-07 08:46:44 +0900

    4

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/': {
+      '/api': {
 
-        target: 'https://routeporter.netlify.app/', // 수정된 API 경로
+        target: 'https://api.routeport.co.kr', // 수정된 API 경로
         changeOrigin: true,
         secure: true, // HTTPS 서버로 프록시하는 경우 필요
         rewrite: (path) => path.replace(/^\/api/, ''), // /api를 제거하여 요청을 /routes로 보냄
