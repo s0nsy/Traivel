2c4f09be158d82e3625e70a7e1d4aaf85378e08f
seyoung <20200927@sungshin.ac.kr>
2025-05-01 23:42:08 +0900

    DOCS: 코드 정리

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -1,8 +1,10 @@
 package com.example.project;
 
+import com.example.project.config.NaverApiConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @MapperScan("com.example.project.mapper")
