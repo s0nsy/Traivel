ea99ad8a5a5f17b9ca0c4bdeb886ab2720b602b7
seyoung <20200927@sungshin.ac.kr>
2025-06-04 01:58:10 +0900

    Feat: 이미지Url

---
+++ src/main/java/com/example/project/entity/dto/ImageDto.java
@@ -0,0 +1,12 @@
+package com.example.project.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageDto {
+   private String ImageUrl;
+}
